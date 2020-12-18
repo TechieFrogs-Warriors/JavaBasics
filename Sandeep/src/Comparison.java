a5eb2213d079e949c28cdb826a8043aabc7c4f19
sandeepmaddipati <saimaddipathi@gmail.com>
2020-12-18 19:45:36 +0530

    Added comparision operators program in the Operation programs

---
+++ Sandeep/src/Comparison.java
@@ -0,0 +1,23 @@
+public class Comparison {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 50;
+        System.out.println("---------comparison operators---------");
+        System.out.println("x > y :" + (x > y));
+        System.out.println("x < y :" + (x < y));
+        System.out.println("x == y :" + (x == y));
+        System.out.println("x != y :" + (x != y));
+        System.out.println("x >= y :" + (x >= y));
+        System.out.println("x <= y :" + (x <= y));
+        int i = 20;
+        int j = 20;
+        System.out.println("i == j :" + (i == j));
+        System.out.println("i != j :" + (i != j));
+        System.out.println("i >= j :" + (i >= j));
+        System.out.println("i <= j :" + (i <= j));
+        System.out.println("i > j :" + (i > j));
+        System.out.println("i < j :" + (i < j));
+
+    }
+
+}
--- Sandeep/src/Operators/Unaryoperator.java
+++ Sandeep/src/Operators/Unaryoperator.java
@@ -13,13 +13,18 @@ public class Unaryoperator {
         System.out.println("Print a:" + a--);
         System.out.println("Print a:" + (--a));
 
-        System.out.println("addition:" + a++ + ++a);
-        System.out.println("addition:" + b++ + b++);
+        System.out.println("addition a:" + (a++ + ++a));
+        System.out.println("addition b:" + (b++ + b++));
+        int i = (((a++) + (++a)) + ((b++) + (++b)));
+        System.out.println("Print i:" + i);
 
         int e = -40;
         int f = 50;
+        byte z = 00011;
+
         System.out.println(~e);
         System.out.println(~f);
+        System.out.println(~z);
         System.out.println(!c);
         System.out.println(!d);
 
