87c1387444965e90c0dc371544bd5bfac515221d
jagathkalyani-ga <jkalyaniee@gmail.com>
2020-12-21 08:30:27 +0530

    variables,operators

---
+++ jagathkalyani/src/Example.java
@@ -0,0 +1,12 @@
+public class Example {
+    public static void main(String[] args){
+        int k = 10;
+        short g = 20;
+        char h = 'b';
+        System.out.println(g);
+        System.out.println(k);
+        System.out.println(h);
+        
+
+    }
+}
