3259cfca1d384a319a452ba6e47848abffffb30c
jagathkalyani-ga <jkalyaniee@gmail.com>
2020-12-21 08:51:45 +0530

    switch statement

---
+++ jagathkalyani/src/Variable.java
@@ -0,0 +1,17 @@
+import javax.lang.model.util.ElementScanner14;
+
+public class Variable {
+    public static void main(String[] args){
+        
+        if ( == 3)
+        System.out.println("=3");
+        else if (k==10)
+        System.out.println("=10");
+        else if (k== -10)
+        System.out.println("= -10");
+        else
+        System.out.println("invalid");
+        
+    }
+    
+}
