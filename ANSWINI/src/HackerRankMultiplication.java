2c19663856cfa191ba65f60f42801661f312c079
answinikk <answinikk99@gmail.com>
2020-12-23 12:53:06 +0530

    Multiplication table program

--- ANSWINI/src/HackerRankMultiplication.java
+++ ANSWINI/src/HackerRankMultiplication.java
@@ -4,12 +4,14 @@ public class HackerRankMultiplication
 {
     public static void main(String[] args) 
     {
+        System.out.println("What Multiplication Table you want : ");
         Scanner obj=new Scanner(System.in);
         int N=obj.nextInt();
         
         int x;
         for(int i=1;i<=10;i++)
         {
+            
             System.out.println(N+" x "+i+" = "+N*i);
             obj.close();
         }
