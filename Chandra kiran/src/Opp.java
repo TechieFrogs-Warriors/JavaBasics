aab4e15ac3bf71e4fdb21adc496eeca723b85455
chadrakiran <chandrakiran.veramachaneni@gmail.com>
2021-01-05 12:43:59 +0530

    34,35,36 Pattern and class,objects pratice prgs

---
+++ Chandra kiran/src/Opp.java
@@ -0,0 +1,19 @@
+ class sample
+ 
+ {
+     public static void main(String[] args) 
+     {
+         
+      
+     Student Stud2=new Student();
+    
+     Stud2.name="kiran";
+     System.out.println(Stud2.rollno);
+     System.out.println(Stud2.name);
+     Stud2.Even();
+     Stud2.Add(300,700,0);
+     
+     }  
+}
+
+
