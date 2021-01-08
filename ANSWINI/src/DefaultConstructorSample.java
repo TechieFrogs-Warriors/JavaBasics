84b5af3ef85496d5c3b41b1b4b896b262c1f8c32
answinikk <answinikk99@gmail.com>
2021-01-08 22:41:27 +0530

    constructor , nested and inner class programs

---
+++ ANSWINI/src/DefaultConstructorSample.java
@@ -0,0 +1,20 @@
+public class DefaultConstructorSample 
+{
+    int num; 
+    String name;
+    boolean value;
+    
+    public static void main(String[] args) 
+    {
+       //default constructor is invoked when creating object to class
+        DefaultConstructorSample obj=new DefaultConstructorSample();
+       System.out.println("INTEGER VALUE IS: "+obj.num);
+       System.out.println("STRING VALUE IS: "+obj.name);
+       System.out.println("BOOLEAN VALUE IS: "+obj.value);
+        
+    }     
+     
+} 
+  
+    
+
