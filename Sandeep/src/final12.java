85b147e2ebb0cc8b3e8803fbf7000803d0bed432
sandeepmaddipati <saimaddipathi@gmail.com>
2021-01-16 09:17:38 +0530

    programs

---
+++ Sandeep/src/final12.java
@@ -0,0 +1,14 @@
+public class final12 {
+    final int speedlimit = 90;// final variable
+
+    void run() {
+        // speedlimit = 400;
+        System.out.println(speedlimit);
+    }
+
+    public static void main(String args[]) {
+        final12 obj = new final12();
+        obj.run();
+    }
+
+}
