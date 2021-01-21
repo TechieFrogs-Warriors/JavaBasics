3cd77150929152c7f41f359294a40e48d97692be
KARTHIKRAJUKANDHULA <karthik901069@gmail.com>
2021-01-21 09:28:34 +0530

    Modfication Inheritance 11 Program

--- KARTHIK/src/InheritancePracticeProgram11.java
+++ KARTHIK/src/InheritancePracticeProgram11.java
@@ -1,19 +1,13 @@
 public class InheritancePracticeProgram11 {
     public static void main(String[] args) {
         Cylinder obj1 = new Cylinder();
-        System.out.println("Radius: " + obj1.getRadius());
-        System.out.println("Height: " + obj1.getHeight());
-        System.out.println("color: " + obj1.getColor());
-        System.out.println("Area: " + obj1.getArea());
-        System.out.println("volume: " + obj1.getvolume());
+        System.out.println("****Circle And Cylinder Detais****"); 
+        System.out.println("Radius: "+obj1.getRadius());
+        System.out.println("Colour: "+obj1.getColor());
+        System.out.println("Height: "+obj1.getHeight());
+        System.out.println("Surface Area of The Cylinder: "+obj1.getArea());
+        System.out.println("Volume Of The Cylinder: "+obj1.getvolume());
         System.out.println(obj1.toString());
-        Cylinder obj2 = new Cylinder(5.0, 2.0, "red");
-        System.out.println("Radius: " + obj2.getRadius());
-        System.out.println("Height: " + obj2.getHeight());
-        System.out.println("color: "+ obj2.getColor());
-        System.out.println("Area: " + obj2.getArea());
-        System.out.println("volume: " + obj2.getvolume());
-        System.out.println(obj2.toString());   
     }  
 }
 class Circle1 {
@@ -27,13 +21,11 @@ class Circle1 {
 
     public Circle1(double radius) {
         this.radius = radius;
-        System.out.println("Radius Of The Circle");
     }
 
     public Circle1(double radius, String color) {
         this.radius = radius;
         this.color = color;
-        System.out.println("Radius And color Of The Circle");
     }
 
     public double getRadius() {
@@ -53,11 +45,12 @@ class Circle1 {
     }
 
     public double getArea() {
+        //System.out.println("Area Of The Circle:");
         return radius * radius * Math.PI;
     }
 
     public String toString() {
-        return "raduis: " + radius +" "+ "color: "+color;
+        return "["+"raduis: " + radius +" "+ "color: "+color+"]";
     }
 
 }
@@ -73,19 +66,16 @@ class Cylinder extends Circle1 {
     public Cylinder(double height) {
         super();
         this.height = height;
-        System.out.println("Height Of The Cylinder");
     }
 
     public Cylinder(double height, double radius) {
         super(radius);
         this.height = height;
-        System.out.println("Height And Radius Of The Cylinder");
     }
 
     public Cylinder(double height, double radius, String color) {
         super(radius, color);
         this.height = height;
-        System.out.println("Height,Radius And Colour Of The Cylinder");
     }
 
     public double getHeight() {
@@ -95,8 +85,15 @@ class Cylinder extends Circle1 {
     public void setHeight(double height) {
         this.height = height;
     }
+    public double getArea(){
+        System.out.println("Area Of The Circle: "+super.getArea());
+        //System.out.println("Surface Area Of The Cylinder:");
+        double SurfaceArea=(2*Math.PI*getRadius()*(getRadius()+getHeight()));
+        return SurfaceArea;
+    }
 
     public double getvolume() {
-        return getArea() * height;
+       // System.out.println("Volume Of The Cylinder:");
+        return super.getArea() * height;
     }
 }
